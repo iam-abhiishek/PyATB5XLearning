da76e4af58334723bc285561560d11b7b8484329
abhishek <abhiishek.kr@gmail.com>
2024-11-17 00:36:47 +0530

    16 Nov code added

---
+++ Nov/ex_16112024_Loops/Lab054.py
@@ -0,0 +1,20 @@
+# LOOPS
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+# Loop
+# Repeat a block of code multiple times.
+
+# for i in range(1, 10): 1 to 9 ( last-1)
+# range( start, stop-1)
+
+for i in range(1, 10):
+    print(i)
