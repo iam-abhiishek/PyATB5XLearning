ea249898ed78d11136d45f64d9a07383b883ac56
abhishek <abhiishek.kr@gmail.com>
2024-11-17 14:15:50 +0530

    code modified

--- Nov/ex_16112024_Loops/Lab068_Task.py
+++ Nov/ex_16112024_Loops/Lab068_Task.py
@@ -4,7 +4,7 @@
 # However, for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz.
 # " For numbers that are multiples of both 3 and 5, print "FizzBuzz." ( for, if)
 
-for i in range(1, 100):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print(i,'FizzBuzz')
     elif i % 3 == 0:
